69383c3009fa29c8dce9662887d50c41e0b3c9d6
Eric Clemmons <eric@smarterspam.com>
2012-09-18 09:50:30 -0500

    skeleton:configure calls skeleton:generate
    Fix #7

--- library/Skeleton/Command/ConfigureSkeletonCommand.php
+++ library/Skeleton/Command/ConfigureSkeletonCommand.php
@@ -4,6 +4,7 @@ namespace Skeleton\Command;
 
 use Skeleton\Console\Helper\DialogHelper;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -68,6 +69,10 @@ class ConfigureSkeletonCommand extends SkeletonCommand
         } else {
             throw new \Exception('Unable to write '.$path);
         }
+
+        $next = $this->getApplication()->find('skeleton:generate');
+
+        $next->run(new ArrayInput(array('command' => 'skeleton:generate')), $output);
     }
 
     protected function interact(InputInterface $input, OutputInterface $output)
