34d4a2a6ab16a96627b8b49a900dc06df8c8a851
Eric Clemmons <eric@smarterspam.com>
2012-09-21 09:27:27 -0500

    Defaulting to N for setting up other environments

--- library/Skeleton/Command/ConfigureSkeletonCommand.php
+++ library/Skeleton/Command/ConfigureSkeletonCommand.php
@@ -84,7 +84,7 @@ class ConfigureSkeletonCommand extends SkeletonCommand
         );
 
         foreach (array('stage', 'prod') as $env) {
-            $continue = $dialog->askConfirmation($output, $dialog->getQuestion(sprintf('Would you like to setup the <info>%s</info> environment?', $env), 'y'));
+            $continue = $dialog->askConfirmation($output, $dialog->getQuestion(sprintf('Would you like to setup the <info>%s</info> environment?', $env), 'n'));
 
             if (!$continue) {
                 continue;
