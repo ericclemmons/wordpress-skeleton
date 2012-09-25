d49bf4572e34f6212bdbdfe0eff107f13387e4e4
Eric Clemmons <eric@smarterspam.com>
2012-09-25 14:08:08 -0500

    Moved cookbooks under skeleton

--- library/Skeleton/Cookbooks/wordpress-skeleton/attributes/default.rb
+++
@@ -1,8 +0,0 @@
-override['mysql']['server_root_password']   = 'vagrant'
-
-default['wordpress']['db']['database']      = "wordpress"
-default['wordpress']['db']['user']          = "vagrant"
-default['wordpress']['db']['password']      = "vagrant"
-default['wordpress']['dir']                 = "/var/www/wordpress-skeleton/current/web"
-default['wordpress']['server_name']         = node['fqdn']
-default['wordpress']['server_aliases']      = [node['fqdn']]
